f0069641db27cc2df0107887f537b96a3cd93a69
IRennie74 <77927518+IRennie74@users.noreply.github.com>
2023-06-24 10:08:49 -0400

    Mouse Safety Check
    I added the detection for mouse movement check on side to side

--- 1.8.9ForgeTemplate-master/src/main/java/com/kingscripts/autoskyblock/autoskyblockmod/macros/garden/SideToSide.java
+++ 1.8.9ForgeTemplate-master/src/main/java/com/kingscripts/autoskyblock/autoskyblockmod/macros/garden/SideToSide.java
@@ -9,52 +9,70 @@ import com.kingscripts.autoskyblock.autoskyblockmod.macros.MacroManager;
 import com.kingscripts.autoskyblock.autoskyblockmod.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 public class SideToSide {
     static Minecraft mc = Minecraft.getMinecraft();
     private static int gameStage = 1;
     private static boolean canRun = true;
+    private static float Yaw;
+    private static float Pitch;
+    private static boolean initiatedMouseSafety = false;
 
     public SideToSide() {
     }
 
     public static void start() {
         MacroManager.scriptIsOn = true;
+        Yaw = Minecraft.getMinecraft().thePlayer.rotationYaw;
+        Pitch = Minecraft.getMinecraft().thePlayer.rotationPitch;
+        initiatedMouseSafety = false;
     }
 
     public static void reset() {
         MacroManager.scriptIsOn = false;
+        initiatedMouseSafety = false;
         gameStage = 1;
     }
 
     public static void main() {
-        if (GardenManager.north_South) {
-            if (Utils.xPositionIsSame) {
+        if(Minecraft.getMinecraft().thePlayer.rotationYaw != Yaw || Minecraft.getMinecraft().thePlayer.rotationPitch != Pitch ) {
+            if(!initiatedMouseSafety) {//initiates mouse safety so that you do not get banned
+                Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "WARNING--MACRO CHECK--WARNING"));
+                Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Initiating mouse check safety"));
+                Utils.disableMovement();
+                initiatedMouseSafety = true;
+            }
+
+        } else {
+            if (GardenManager.north_South) {
+                if (Utils.xPositionIsSame) {
+                    ++gameStage;
+                    Utils.disableMovement();
+                    Utils.xPositionIsSame = false;
+                    canRun = true;
+                }
+            } else if (Utils.zPositionIsSame) {
                 ++gameStage;
                 Utils.disableMovement();
-                Utils.xPositionIsSame = false;
+                Utils.zPositionIsSame = false;
                 canRun = true;
             }
-        } else if (Utils.zPositionIsSame) {
-            ++gameStage;
-            Utils.disableMovement();
-            Utils.zPositionIsSame = false;
-            canRun = true;
-        }
 
-        if (canRun) {
-            if (gameStage % 2 != 0) {
-                //Minecraft.getMinecraft().thePlayer.sendChatMessage("/sethome");
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), true);
-                canRun = false;
-            } else {
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), true);
-                canRun = false;
+            if (canRun) {
+                if (gameStage % 2 != 0) {
+                    //Minecraft.getMinecraft().thePlayer.sendChatMessage("/sethome");
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), true);
+                    canRun = false;
+                } else {
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
+                    KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), true);
+                    canRun = false;
+                }
             }
         }
-
     }
 
     public static void safetyRestart() {
