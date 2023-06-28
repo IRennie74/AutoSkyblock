66095e3df8aae6e1b7ecf3ab32a2094634910ef5
IRennie74 <77927518+IRennie74@users.noreply.github.com>
2023-06-28 18:42:05 -0400

    Added Going to npc and bazaar

--- 1.8.9ForgeTemplate-master/src/main/java/com/kingscripts/autoskyblock/autoskyblockmod/macros/garden/WheatInHub.java
+++ 1.8.9ForgeTemplate-master/src/main/java/com/kingscripts/autoskyblock/autoskyblockmod/macros/garden/WheatInHub.java
@@ -28,128 +28,73 @@ import static java.lang.Math.round;
 public class WheatInHub {
     static Minecraft mc = Minecraft.getMinecraft();
     public static int gameStage = 1;
-    private static boolean useKeyStrokes = false;
-    private static int count = 0;
     private static int count1 = 0;
-    private static int count2 = 0;
-    private static int count3 = 0;
-    private static int count4 = 0;
-    private static int count5 = 0;
-    private static int count6 = 0;
-    private static int count7 = 0;
-    private static int count8 = 0;
-    private static int count9 = 0;
-    private static int count10 = 0;
-    private static int count11 = 0;
-    private static int count12 = 0;
-    private static int count13 = 0;
-    private static int count14 = 0;
-    private static int count15 = 0;
-    private static int count16 = 0;
-    private static int count17 = 0;
-    private static int count18 = 0;
-    private static int count19 = 0;
-    private static int count20 = 0;
-    private static int count21 = 0;
-    private static int count22 = 0;
-    private static int count23 = 0;
-    private static int count24 = 0;
-    private static int count25 = 0;
-    private static int count26 = 0;
-    private static int count27 = 0;
-    private static int count28 = 0;
-    private static int count29 = 0;
-    private static int count30 = 0;
-    private static int count31 = 0;
-    private static int count32 = 0;
-    private static int count33 = 0;
-    private static int count34 = 0;
-    private static int count35 = 0;
-    private static int count36 = 0;
-    private static boolean convertScript = false;
-    private static boolean checkForStop = false;
-    private static boolean checkForCamera = false;
-    private static boolean canRunMath = true;
-    private static boolean useLerp2;
-    private static double currentStopX;
-    private static double currentStopZ;
-    private static int xStopDirection;
-    private static int zStopDirection;
-    private static float yawAdd;
-    private static float pitchAdd;
-    private static String Yaw;
-    private static String Pitch;
-    private static char c;
-    private static String d;
-    private static int yawL;
-    private static int pitchL;
-    private static String data1;
-    public static double delay = 2;
-    private static double delayNumber;
 
-    private static float lerp = 0.02f;
-    private static int timesAdded = 0;
     private static int hubLevel = 1;
-    private static long startMillis = 0;
     private static boolean hasSavedMillis = false;
     private static boolean thisScriptOn = false;
     public static boolean pasting = true;
-    private static long lastSavedDivision;
     private static Instant watch1;
     private static Instant watch2;
 
 
     public WheatInHub() {
     }
-    public static String getWheatHub(int wheatLevel,int count1, int level){
-        if(wheatLevel == 1)return WheatHub.movement[count1][level];
-        else if(wheatLevel == 2)return WheatHub2.movement2[count1][level];
-        else return null;
+    public static String getWheatHub(int whichFarm,int wheatLevel,int count1, int level){
+        if(whichFarm == 1) {//runs farming
+            if (wheatLevel == 1) return WheatHub.movement[count1][level];
+            else if (wheatLevel == 2) return WheatHub2.movement2[count1][level];
+        } else if (whichFarm == 2) {//runs getting to npc and opening
+            return NPC.movement2[count1][level];
+        } else if (whichFarm == 3) {//runs going to bazaar and opening
+            return Bazaar.movement2[count1][level];
+        }
+        return null;
     }
-    public static void execute(int hubLevel,int count1) {
-        Minecraft.getMinecraft().thePlayer.rotationYaw = Float.parseFloat(getWheatHub(hubLevel, count1, 0));
-        Minecraft.getMinecraft().thePlayer.rotationPitch = Float.parseFloat(getWheatHub(hubLevel, count1, 1));
-        if (getWheatHub(hubLevel, count1, 2) == "t") {//jump
+    public static void execute(int whichFarm, int hubLevel,int count1) {
+        Minecraft.getMinecraft().thePlayer.rotationYaw = Float.parseFloat(getWheatHub(whichFarm, hubLevel, count1, 0));
+        Minecraft.getMinecraft().thePlayer.rotationPitch = Float.parseFloat(getWheatHub(whichFarm,hubLevel, count1, 1));
+        if (getWheatHub(whichFarm, hubLevel, count1, 2) == "t") {//jump
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindJump.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindJump.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 3) == "t") {//sneak
+        if (getWheatHub(whichFarm, hubLevel, count1, 3) == "t") {//sneak
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 4) == "t") {//sprint
+        if (getWheatHub(whichFarm, hubLevel, count1, 4) == "t") {//sprint
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 5) == "t") {//left
+        if (getWheatHub(whichFarm, hubLevel, count1, 5) == "t") {//left
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 6) == "t") {//right
+        if (getWheatHub(whichFarm, hubLevel, count1, 6) == "t") {//right
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 7) == "t") {//back
+        if (getWheatHub(whichFarm, hubLevel, count1, 7) == "t") {//back
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindBack.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindBack.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 8) == "t") {//forward
+        if (getWheatHub(whichFarm, hubLevel, count1, 8) == "t") {//forward
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 9) == "t") {//attack
+        if (getWheatHub(whichFarm, hubLevel, count1, 9) == "t") {//attack
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
         }
-        if (getWheatHub(hubLevel, count1, 10) == "t") {//useitem
+        if (getWheatHub(whichFarm, hubLevel, count1, 10) == "t") {//useitem
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), true);
         } else {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
@@ -166,16 +111,29 @@ public class WheatInHub {
                     watch2 = Instant.now();
                     if ((Duration.between(watch1, watch2).toMillis()) >= 10) {
                         hasSavedMillis = false;
-                        execute(hubLevel, count1);
-                        if (count1 >= 909 && hubLevel == 1) {
-                            count1 = 0;
-                            hubLevel++;
-                        }else if (count1 >= 809 && hubLevel == 2) {
-                            count1 = 0;
-                            hubLevel = 1;
-                            Minecraft.getMinecraft().thePlayer.sendChatMessage("/warp hub");
+                        if(gameStage == 1) {//farms crops and brings you back to hub spawn
+                            execute(1,hubLevel, count1);
+                            if (count1 >= 909 && hubLevel == 1) {
+                                count1 = 0;
+                                hubLevel++;
+                            } else if (count1 >= 809 && hubLevel == 2) {
+                                count1 = 0;
+                                hubLevel = 1;
+                                gameStage++;
+                                Minecraft.getMinecraft().thePlayer.sendChatMessage("/warp hub");
+                            }
+                            count1++;
+                        } else if(gameStage == 2){//Brings you to npc
+                            if (count1 <= 159 && hubLevel == 1) {//runs script
+                                execute(2,1,count1);
+                            } else  {
+                                count1 = 0;
+                                gameStage++;
+                            }
+                            count1++;
+                        } else if(gameStage == 3){//sells inventory and brings you back to hub spawn
+
                         }
-                        count1++;
                     }
                 } else {
                 if(!hasSavedMillis){
@@ -219,8 +177,6 @@ public class WheatInHub {
                         out.close();
                     } catch (IOException ignored) {
                     }
-//                    count++;
-//                    delayNumber++;
                 }
                 }
             }
@@ -229,30 +185,18 @@ public class WheatInHub {
     public static void start() {
         MacroManager.scriptIsOn = true;
         Utils.resetPositionIsSame();
-        checkForStop = false;
-        checkForCamera = false;
-        canRunMath = true;
-        timesAdded = 0;
         gameStage = 1;
         hubLevel = 1;
-        count = 0;
         count1 = 0;
-        startMillis = 0;
         hasSavedMillis = false;
     }
 
     public static void reset() {
         MacroManager.scriptIsOn = false;
-        canRunMath = true;
         gameStage = 1;
-        timesAdded = 0;
         hubLevel = 1;
-        count = 0;
         count1 = 0;
         Utils.resetPositionIsSame();
-        checkForStop = false;
-        checkForCamera = false;
-        startMillis = 0;
         hasSavedMillis = false;
     }
     public static void main(){
